f7813bc9a2a4d32a45e24d83930177260512645d
Tim Caswell <tim@creationix.com>
2009-11-28 23:27:16 -0600

    Update the sample to be more generic.

--- sample.js
+++ sample.js
@@ -1,7 +1,7 @@
 var sys = require("sys");
 var Postgres = require('./postgres')
 
-var db = new Postgres.Connection("land", "enterprisedb", "enterprisedb");
+var db = new Postgres.Connection("database", "username", "password");
 process.ARGV.slice(2).forEach(function (sql) {
   sys.puts(sql)
   db.query(sql, function (data) {
