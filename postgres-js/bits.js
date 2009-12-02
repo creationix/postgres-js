2d9b674005f9e3f1e11ddd4d703c8c587291c48f
Tim Caswell <tim@creationix.com>
2009-12-02 14:48:50 -0600

    Rewrite bits.js to use builder and consumer objects instead of raw strings.  Now we don't have to clobber the String object.

--- postgres-js/bits.js
+++ postgres-js/bits.js
@@ -4,94 +4,131 @@
 // streams which are encoded as javascript strings
 // that only use the first 8 bits of each character.
 
-// Quick alias to shorten call time and to shorten code
-var chr = String.fromCharCode;
+// exports.Encoder - A binary string builder object.
+(function () {
 
-// Encode number as 32 bit 2s compliment
-String.prototype.add_int32 = function (number) {
-  var a, b, c, d, unsigned;
-  unsigned = (number < 0) ? (number + 0x100000000) : number;
-  a = Math.floor(unsigned / 0xffffff);
-  unsigned &= 0xffffff;
-  b = Math.floor(unsigned / 0xffff);
-  unsigned &= 0xffff;
-  c = Math.floor(unsigned / 0xff);
-  unsigned &= 0xff;
-  d = Math.floor(unsigned);
-  return this + chr(a) + chr(b) + chr(c) + chr(d);
-};
+  var chr, proto;
+  
+  exports.Encoder = function () {
+    this.data = "";
+  };
 
-// Encode number as 16 bit 2s compliment
-String.prototype.add_int16 = function (number) {
-  var a, b, unsigned;
-  unsigned = (number < 0) ? (number + 0x10000) : number;
-  a = Math.floor(unsigned / 0xff);
-  unsigned &= 0xff;
-  b = Math.floor(unsigned);
-  return this + chr(a) + chr(b);
-};
+  chr = String.fromCharCode;
+  proto = exports.Encoder.prototype;
+  
+  // Factor out the encode so it can be shared by add_header and push_int32
+  function encode_int32(number) {
+    var a, b, c, d, unsigned;
+    unsigned = (number < 0) ? (number + 0x100000000) : number;
+    a = Math.floor(unsigned / 0xffffff);
+    unsigned &= 0xffffff;
+    b = Math.floor(unsigned / 0xffff);
+    unsigned &= 0xffff;
+    c = Math.floor(unsigned / 0xff);
+    unsigned &= 0xff;
+    d = Math.floor(unsigned);
+    return chr(a) + chr(b) + chr(c) + chr(d);
+  }
 
-// Encode string without null terminator
-String.prototype.add_raw_string = function (text) {
-  return this + text;
-};
-
-// Encode text as null terminated string
-String.prototype.add_cstring = function (text) {
-  return this + text + "\0";
-};
-
-// Encode as a null terminated array of cstrings
-String.prototype.add_multi_cstring = function (fields) {
-  return this + fields.join("\0") + "\0\0";
-};
-
-// Convert 4 characters to signed 32 bit integer
-String.prototype.parse_int32 = function () {
-  var unsigned = this.charCodeAt(0) * 0x1000000 + this.charCodeAt(1) * 0x10000 + this.charCodeAt(2) * 0x100 + this.charCodeAt(3);
-  return [4, (unsigned & 0x80000000) ? (unsigned - 0x100000000) : unsigned];
-};
-
-// Convert 2 bytes to signed 16 bit integer
-String.prototype.parse_int16 = function () {
-  var unsigned = this.charCodeAt(0) * 0x100 + this.charCodeAt(1);
-  return [2, (unsigned & 0x8000) ? (unsigned - 0x10000) : unsigned];
-};
-
-// Grab number of bytes as a string
-String.prototype.parse_raw_string = function (len) {
-  return [len, this.substr(0, len)];
-};
-
-// Grab a null terminated string from the this
-String.prototype.parse_cstring = function () {
-  var pos = this.indexOf("\0");
-  return [pos + 1, this.substr(0, pos)];
-};
-
-// Grab a null terminated array of null terminated strings
-String.prototype.parse_multi_cstring = function () {
-  var pos = this.indexOf("\0\0");
-  return [pos + 2, this.substr(0, pos).split("\0")];
-};
-
-// Takes any number of commands to tell how to parse the binary input.
-String.prototype.parse = function () {
-  var pos, self, data;
-  pos = 0;
-  self = this;
-  data = Array.prototype.map.call(arguments, function (command) {
-    var args, pair;
-    if (typeof command === 'string') {
-      args = [];
-    } else {
-      args = command.slice(1);
-      command = command[0];
+  // Add a postgres header to the binary string
+  proto.add_header = function (code) {
+    if (code === undefined) {
+      code = "";
     }
-    pair = String.prototype["parse_" + command].apply(self.substr(pos), args);
-    pos += pair[0];
-    return pair[1];
-  });
-  return [pos, data];
-};
+    this.data = code + encode_int32(this.data.length + 4) + this.data;
+    return this;
+  };
+
+  // Encode number as 32 bit 2s compliment
+  proto.push_int32 = function (number) {
+    this.data += encode_int32(number);
+    return this;
+  };
+
+  // Encode number as 16 bit 2s compliment
+  proto.push_int16 = function (number) {
+    var a, b, unsigned;
+    unsigned = (number < 0) ? (number + 0x10000) : number;
+    a = Math.floor(unsigned / 0xff);
+    unsigned &= 0xff;
+    b = Math.floor(unsigned);
+    this.data += chr(a) + chr(b);
+    return this;
+  };
+
+  // Encode string without null terminator
+  proto.push_raw_string = function (text) {
+    this.data += text;
+    return this;
+  };
+
+  // Encode text as null terminated string
+  proto.push_cstring = function (text) {
+    this.data += text + "\0";
+    return this;
+  };
+
+  // Encode as a null terminated array of cstrings
+  proto.push_multi_cstring = function (fields) {
+    this.data += fields.join("\0") + "\0\0";
+    return this;
+  };
+
+}());
+
+// exports.Decoder - A binary string consumer object.
+// TODO: Convert to use a moving pointer instead of creating a new substring
+//       each iteration.  This will help performance a bit on parsing.
+(function () {
+  
+  var proto;
+  
+  exports.Decoder = function (data) {
+    this.data = data;
+  };
+
+  proto = exports.Decoder.prototype;
+
+  // Convert 4 characters to signed 32 bit integer
+  proto.shift_int32 = function () {
+    var unsigned = this.data.charCodeAt(0) * 0x1000000 + this.data.charCodeAt(1) * 0x10000 + this.data.charCodeAt(2) * 0x100 + this.data.charCodeAt(3);
+    this.data = this.data.substr(4);
+    return (unsigned & 0x80000000) ? (unsigned - 0x100000000) : unsigned;
+  };
+
+  // Convert 2 bytes to signed 16 bit integer
+  proto.shift_int16 = function () {
+    var unsigned = this.data.charCodeAt(0) * 0x100 + this.data.charCodeAt(1);
+    this.data = this.data.substr(2);
+    return (unsigned & 0x8000) ? (unsigned - 0x10000) : unsigned;
+  };
+
+  // Grab number of bytes as a string
+  proto.shift_raw_string = function (len) {
+    var string = this.data.substr(0, len);
+    this.data = this.data.substr(len);
+    return string;
+  };
+  
+  // Grab a null terminated string
+  proto.shift_cstring = function () {
+    var pos, string;
+    pos = this.indexOf("\0");
+    string = this.data.substr(0, pos);
+    this.data = this.data.substr(pos + 1);
+    return string;
+  };
+  
+  // Grab a null terminated array of null terminated strings
+  proto.shift_multi_cstring = function () {
+    var pos, string;
+    pos = this.data.indexOf("\0\0");
+    string = this.data.substr(0, pos).split("\0");
+    this.data = this.data.substr(pos + 1);
+    return string;
+  };
+  
+
+}());
+
 
